7a39760de38e8697be8277a5ff7c1a79be7bc4a9
Kyungjin0 <zhanganthony14@gmail.com>
2025-02-13 10:38:46 +0100

    pokedex ( affichage des cartes ) + barre de recherche

---
+++ src/component/carte/Carte.jsx
@@ -0,0 +1,53 @@
+import "../../style/Carte.css";
+import {useState, useEffect} from "react";
+
+
+const Carte = ({ pokemon }) => {
+  return (
+    <div className="pokemon-card" data-type={pokemon.type[0]}>
+      {/* En-tête : Nom et Type */}
+      <div className="card-header">
+        <div className="header-left">
+          <h1 className="pokemon-name">{pokemon.name.french}</h1>
+        </div>
+        <div className="header-right">
+          <img src={`./src/assets/types/${pokemon.type[0]}.png`} alt={pokemon.type[0]} className="type-icon-header" />
+        </div>
+      </div>
+
+      {/* Image du Pokémon */}
+      <div className="card-image-container">
+        <img src={pokemon.image} alt={pokemon.name.french} className="pokemon-image" />
+      </div>
+
+      {/* Section des stats */}
+      <div className="card-stats">
+        <h2 className="stats-title">Stats</h2>
+        <div className="stats-grid">
+          <div className="stat-block">
+            <span>🧡{pokemon.base.HP}</span>
+          </div>
+          <div className="stat-block">
+            <span>🗡️{pokemon.base.Attack}</span>
+          </div>
+          <div className="stat-block">
+        
+            <span>🛡{pokemon.base.Defense}</span>
+            
+          </div>
+          <div className="stat-block">
+            <span>⚔️{pokemon.base["Sp. Attack"]}</span>
+          </div>
+          <div className="stat-block">
+            <span>🛡️{pokemon.base["Sp. Defense"]}</span>
+          </div>
+          <div className="stat-block">
+            <span>👟{pokemon.base.Speed}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carte;
